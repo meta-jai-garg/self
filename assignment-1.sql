d13a0bc4b2a0f233d573e9573c9f09fcd35f54f0
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-20 18:33:27 +0530

    Add files via upload

--- assignment-1.sql
+++ assignment-1.sql
@@ -1,143 +1,33 @@
-/*
-Creating Database
-*/
-CREATE DATABASE STOREFRONT;
+-- Create a function to calculate number of orders in a month. Month and year will be input parameter to function
+DROP FUNCTION if exists no_of_orders_in_month;
+DROP FUNCTION if exists max_order_month;
 
-use storefront;
-/*
-Creating user table
-*/
+DELIMITER |
+CREATE FUNCTION no_of_orders_in_month(month INT, year INT) RETURNS INT 
+    DETERMINISTIC
+        BEGIN
+            DECLARE order_count INT;
+                    Select count(*) INTO order_count from product_order
+                    where EXTRACT(MONTH FROM order_date) = month AND EXTRACT(YEAR FROM order_date) = year;
+        RETURN order_count;
+    END |
+    
+select no_of_orders_in_month(7, 2018);
 
-CREATE TABLE user(
-    user_id INT AUTO_INCREMENT,
-    user_name VARCHAR(100) NOT NULL,
-    user_type CHAR(1) NOT NULL DEFAULT 'S',
-    registration_date DATE NOT NULL,
-    PRIMARY KEY (user_id)
-);
+-- Create a function to return month in a year having maximum orders. Year will be input parameter.
 
-/*
-creating address table
-*/
-
-CREATE TABLE address(
-    address_id INT AUTO_INCREMENT,
-    user_id INT,
-    phone VARCHAR(10) NOT NULL,
-    house_no VARCHAR(10) NOT NULL,
-    street VARCHAR(20),
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(20) NOT NULL,
-    country VARCHAR(50) NOT NULL,
-    zipcode INT(6) NOT NULL,
-    PRIMARY KEY (address_id),
-    FOREIGN KEY (user_id)
-        REFERENCES user (user_id)
-);
-
-/*
-creating product table
-*/
-CREATE TABLE product(
-    product_id INT AUTO_INCREMENT,
-    product_name VARCHAR(50) NOT NULL,
-    product_price FLOAT NOT NULL,
-    stock INT,
-    added_date DATE NOT NULL,
-    modify_date DATE,
-    PRIMARY KEY (product_id)
-);
-
-/*
-creating category table
-*/
-
-CREATE TABLE category(
-    category_id INT AUTO_INCREMENT,
-    category_name varchar(30) NOT NULL,
-    parent_categopry varchar(30),
-    PRIMARY KEY (category_id)
-);
-
-/*
-creating category relation
-*/
-CREATE TABLE category_relation(
-    category_id INT,
-    product_id INT,
-    PRIMARY KEY (category_id,product_id),
-    FOREIGN KEY (category_id)
-        REFERENCES category (category_id),
-    FOREIGN KEY (product_id)
-        REFERENCES product (product_id)
-);
-/*
-creating table for product image
-*/
-
-CREATE TABLE product_image(
-    img_id INT AUTO_INCREMENT,
-    img_url varchar(300) NOT NULL,
-    img_name varchar(50) NOT NULL,
-    product_id INT,
-    PRIMARY KEY (img_id),
-    FOREIGN KEY (product_id)
-        REFERENCES product (product_id)
-);
-
-/*
-creating table for product order
-*/
-CREATE TABLE product_order(
-    order_id INT AUTO_INCREMENT,
-    address_id INT,
-    PRIMARY KEY (order_id),
-    FOREIGN KEY (address_id)
-        REFERENCES address (address_id)
-);
-
-ALTER TABLE product_order
-ADD COLUMN order_date DATE;
-
-/*
-creating table for order detail
-*/
-CREATE TABLE order_detail(
-    detail_id INT AUTO_INCREMENT,
-    order_id INT REFERENCES product_order (order_id),
-    amount double NOT NULL,
-    status ENUM('SHIPPED', 'CANCELLED', 'RETURNED'),
-    last_update TIMESTAMP NOT NULL,
-    PRIMARY KEY (detail_id)
-);
-
--- renaming order_detail table
-RENAME TABLE order_detail TO product_order_detail;
-
--- adding column to product order detail
-ALTER TABLE product_order_detail
-ADD COLUMN product_id INT REFERENCES product(product_id) after order_id;
-
-
-
-/*command to display all the table names present in StoreFront*/
-show tables;
-
-/*command to remove Product table of the StoreFront database*/
-SET FOREIGN_KEY_CHECKS = 0;
-drop table product;
-SET FOREIGN_KEY_CHECKS = 1;
-
-/*command to display all the table names present in StoreFront*/
-show tables;
-
-/*Again creation of product table*/
-CREATE TABLE product(
-    product_id INT AUTO_INCREMENT,
-    product_name VARCHAR(50) NOT NULL,
-    product_price FLOAT NOT NULL,
-    stock INT,
-    added_date DATE NOT NULL,
-    modify_date DATE,
-    PRIMARY KEY (product_id)
-);
+DELIMITER |
+CREATE FUNCTION max_order_month(in_year INT) RETURNS INT 
+    DETERMINISTIC
+        BEGIN
+            DECLARE max_order_month INT;
+                    Select MONTH(order_date) INTO max_order_month
+                    from product_order
+                    where YEAR(order_date) in (in_year)
+                    GROUP BY MONTH(order_date)
+                    ORDER BY COUNT(*) DESC
+                    LIMIT 1;
+        RETURN max_order_month;
+    END |
+    
+select max_order_month(2018);
